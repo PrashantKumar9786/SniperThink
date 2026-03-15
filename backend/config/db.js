fb0aa651b63b39539ca385cd855743eb4dcfc7ab
PrashantKumar9786 <prashantkushwah913@gmail.com>
2026-03-16 00:53:19 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,12 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Required for Render PostgreSQL
+    },
+  },
 });
 
 module.exports = sequelize;
